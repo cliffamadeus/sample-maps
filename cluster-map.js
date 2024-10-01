e050dc5d6ff205b857b9083071d35024d6140e3a
cliffamadeus <reconcliffy@gmail.com>
2024-10-01 16:53:08 +0800

    Added cluster map resources

---
+++ cluster-map.js
@@ -0,0 +1,16 @@
+class LeafletMap {
+    constructor(containerId, center, zoom) {
+        this.map = L.map(containerId).setView(center, zoom);
+        this.initTileLayer();
+        this.markerClusterGroup = L.markerClusterGroup().addTo(this.map);
+    }
+
+    initTileLayer() {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+    }
+}
+
+const myMap = new LeafletMap('map', [8.360004, 124.868419], 16);
